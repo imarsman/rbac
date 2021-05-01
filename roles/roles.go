555cd45aa6e923292b8e9d32d8682dc433fabe52
Ian Marsman <ian@xmlteam.com>
2021-05-01 12:18:42 -0400

    Clarification

--- roles/roles.go
+++ roles/roles.go
@@ -8,14 +8,14 @@ func init() {
 }
 
 // checkEnforce check a role against rules
-func checkEnforce(e *casbin.Enforcer, sub string, obj string, act string) bool {
+func checkEnforce(e *casbin.Enforcer, sub, obj, act string) bool {
 	return e.Enforce(sub, obj, act)
 }
 
 // CheckAllowForRoles check object and action against list of groups
 func CheckAllowForRoles(e *casbin.Enforcer, obj string, act string, roles ...string) bool {
 	for _, r := range roles {
-		pass := checkEnforce(e, r, obj, act)
+		var pass bool = checkEnforce(e, r, obj, act)
 		if pass == true {
 			return true
 		}
